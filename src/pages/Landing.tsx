49c3c90ad4c275440996815f8f8861f532e3b485
Allen Nguyen <anhn321@gmail.com>
2025-04-10 23:29:01 -0700

    feat: styling hero

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -19,35 +19,33 @@ export default function Landing() {
 }
 
 function Hero() {
+	function openSignUpModal() {
+		(document.getElementById('sign-up-modal') as HTMLDialogElement).showModal();
+	}
+
 	return (
 		<div
-			className='blurred-edges hero min-h-[calc(99lvh-4rem)] max-w-screen-xl'
+			className='blurred-edges hero h-[600px] max-w-screen-xl'
 			style={{
 				backgroundImage:
 					'url(https://media.cnn.com/api/v1/images/stellar/prod/130907221429-jukebox-1942.jpg?q=w_3580,h_2340,x_0,y_0,c_fill)',
 			}}
 		>
 			<div className='hero-overlay bg-opacity-40'></div>
-			<div className='hero-content mb-10 items-end text-center'>
-				<div className='flex flex-col items-center justify-center gap-6'>
-					<p className='mb-5 font-playfair text-5xl font-bold text-white'>
-						Track albums you&apos;ve listened to.
-						<br />
-						Save those you want to hear.
-						<br />
-						Tell your friends what&apos;s good.
-					</p>
-					<button
-						className='btn btn-primary btn-wide text-lg font-semibold text-white'
-						onClick={() =>
-							(
-								document.getElementById('sign-up-modal') as HTMLDialogElement
-							).showModal()
-						}
-					>
-						Get started — it&apos;s free!
-					</button>
-				</div>
+			<div className='hero-content mb-16 h-full flex-col justify-end p-0 text-center'>
+				<p className='font-playfair text-4xl font-bold text-white'>
+					Track albums you&apos;ve listened to.
+					<br />
+					Save those you want to hear.
+					<br />
+					Tell your friends what&apos;s good.
+				</p>
+				<button
+					className='btn btn-primary btn-wide text-lg font-semibold text-white'
+					onClick={openSignUpModal}
+				>
+					Get started — it&apos;s free!
+				</button>
 			</div>
 			<SignUpModal />
 		</div>
