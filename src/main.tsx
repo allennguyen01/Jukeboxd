7fe482235c8d3496e82f031e415c63ff0fa9211d
Allen Nguyen <anhn321@gmail.com>
2025-04-18 16:10:29 -0700

    feat: implement ThemeProvider for default dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from './components/theme-provider';
 import App from './App.jsx';
 import './index.css';
 
@@ -9,9 +10,11 @@ const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<QueryClientProvider client={queryClient}>
-			<App />
-			<ReactQueryDevtools initialIsOpen={false} />
-		</QueryClientProvider>
+		<ThemeProvider defaultTheme='dark'>
+			<QueryClientProvider client={queryClient}>
+				<App />
+				<ReactQueryDevtools initialIsOpen={false} />
+			</QueryClientProvider>
+		</ThemeProvider>
 	</StrictMode>,
 );
