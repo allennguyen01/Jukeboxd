a95eba5ab614fb837629477beff1df7e38c9c398
Allen Nguyen <anhn321@gmail.com>
2025-08-22 00:08:29 -0700

    docs: add supabase types export file

--- src/types/supabaseTypes.ts
+++ src/types/supabaseTypes.ts
@@ -1,9 +1,5 @@
-export type AlbumReview = {
-	id: string;
-	user_id: string;
-	created_at: Date;
-	album_name: string;
-	rating: number;
-	review: string;
-	listened: boolean;
-};
+import { Database } from './supabase';
+
+export type AlbumReview = Database['public']['Tables']['reviews']['Row'];
+
+export type UserProfile = Database['public']['Tables']['profiles']['Row'];
